0fb5d571230bb1b670cb4084754ba065c2539449
saran2307 <saran.nghz@gmail.com>
2017-08-21 14:40:46 +0530

    Toast added

--- app/src/main/java/sg/myapp/MainActivity.java
+++ app/src/main/java/sg/myapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +21,11 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    Toast.makeText(getApplicationContext(), "home clicked", Toast.LENGTH_LONG).show();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
